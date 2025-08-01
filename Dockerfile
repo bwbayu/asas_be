59abc1c30bc5e06111484033eb4e8cefe3ffdee9
Bayu Wicaksono <bwbayuuu@gmail.com>
2025-08-01 21:11:09 +0700

    modify some files

--- Dockerfile
+++ Dockerfile
@@ -4,12 +4,13 @@ WORKDIR /app
 
 COPY requirements.txt .
 
-RUN apt-get update && apt-get install -y curl
-RUN pip install torch --index-url https://download.pytorch.org/whl/cpu
-RUN pip install -r requirements.txt
+RUN apt-get update && \
+    apt-get install -y curl nginx && \
+    pip install torch --index-url https://download.pytorch.org/whl/cpu && \
+    pip install -r requirements.txt
 
 COPY . .
 
-EXPOSE 8000
+EXPOSE 8080
 
-CMD [ "gunicorn", "-w", "1", "-b", "0.0.0.0:8000", "app:app", "--access-logfile", "-", "--timeout", "120"]
+CMD [ "gunicorn", "-w", "1", "-b", "0.0.0.0:8080", "app:app", "--access-logfile", "-", "--timeout", "120"]
--- client.py
+++ client.py
@@ -1,12 +1,35 @@
 import json
 import requests
+import time
+
+# Configuration
+url = 'http://localhost:5000/score'
+headers = {
+    'Content-Type': 'application/json',
+    'X-Forwarded-For': f"1.2.3.1"  # simulasi 40 IP berbeda
+}
 
 data = {
     'answer': "asdfasdfasdf",
     'reference': "gatau"
 }
 
-url = 'http://localhost:81/score'
-headers = {'Content-Type': 'application/json'}  # Add this header
-response = requests.post(url, data=json.dumps(data), headers=headers)
-print(response.json())
+success_count = 0
+fail_count = 0
+
+start_time = time.time()
+
+for _ in range(40):
+    try:
+        response = requests.post(url, data=json.dumps(data), headers=headers, timeout=10)
+        if response.status_code == 200:
+            success_count += 1
+        else:
+            fail_count += 1
+    except Exception:
+        fail_count += 1
+
+end_time = time.time()
+total_time = end_time - start_time
+
+(success_count, fail_count, total_time)
